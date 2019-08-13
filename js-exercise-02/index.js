0eee5d48a326162d2b245d962cf1fb07b2749a36
Sunthief <johannes.kneussel@googlemail.com>
2019-08-13 18:34:53 +0200

    Add exercises day 2

---
+++ js-exercise-02/index.js
@@ -0,0 +1,82 @@
+//1
+
+const firstNumber = 17;
+const secondNumber = 30;
+const percentage = (firstNumber / secondNumber) * 100;
+
+console.log(percentage+"%"); //What are dollar sign, backtick and curly bracktes for?
+
+
+//2
+
+const workingHours = 8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4;
+const workingDays = 10;
+console.log(`2) Noemy's average hours are: ${workingHours / workingDays}`); // 5.85 hours
+
+//3
+
+let z = (85*6)-(75+70+85+90+100)
+console.log(`3) Score in the sixth test: ${z}`);
+
+//4
+
+let totalBeforeFinalExam = 8*78
+let totalNumberOfExam = 9
+let goalAverage = 80
+let goalForFinalExam =(totalNumberOfExam*goalAverage)-(totalBeforeFinalExam)
+console.log(`4) James needs a minimum of ${goalForFinalExam}% to get an 80% average.`);
+
+//5
+
+const billOne = 22.35;
+const tipOne = billOne * 0.10;
+const billTwo = 26.67;
+const tipTwo = billTwo * 0.15;
+const billThree = 35.92;
+const tipThree = billThree * 0.20;
+
+console.log(`5a) The total amount of the first bill including 10% tip is: ${billOne + tipOne}`); // 24.58
+console.log(`5b) The total amount of the second bill including 15% tip is: ${billTwo + tipTwo}`); // 30.67
+console.log(`5c) The total amount of the third bill including 20% tip is: ${billThree + tipThree}`); // 43.10
+
+// 6a - Perimeter of a square
+
+const sideLength = 4.75;
+const sides = 4;
+
+console.log(`The perimeter of this square is ${sideLength * sides}cm.`);
+
+//6b
+
+const sideOne = 5;
+const sideTwo = 6;
+const sideThree = 7;
+
+console.log(`The perimeter of this triangle is ${sideOne + sideTwo + sideThree}cm.`);
+
+
+//6c 
+
+const sideSquare = 5;
+const totalSides = 4;
+
+console.log(`The area of this square is ${sideSquare * totalSides}cm.`); // 20cm
+
+//6d
+
+let areaTriangle = (5 + 6 + 7)/2
+console.log(`The area of the triangle is ${areaTriangle}`);
+
+//6e
+
+let volumeOfCube = 9*9*9
+console.log(`${volumeOfCube}`);
+
+//7
+
+let ticket = 6450 / 15
+console.log(`${ticket} people purchased a ticket.`);
+
+
+let yearlyIncome = 500 * 52
+console.log(`${yearlyIncome}`);
