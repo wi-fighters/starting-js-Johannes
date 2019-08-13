629f06d2ef55181067e3372d82727e3f0daa7dff
Sunthief <johannes.kneussel@googlemail.com>
2019-08-13 18:03:52 +0200

    Add more exercises

--- js-exercise-01/index.js
+++ js-exercise-01/index.js
@@ -1,5 +1,48 @@
+//1
 var firstName;
 firstName = "Johannes";
 var lastName;
 lastName = "Kneussel";
 console.log(firstName,lastName);
+
+//2
+var name = "Johannes";
+var city = "Berlin";
+var job = "teacher";
+var age = "34";
+var family = "single"
+console.log(name,city,job,age,family)
+
+//3
+console.log(name + " " + "Smith" + " " + "is" + " " + "a" + " " + "43" + " "+ "year" + " " + "old" + " " + "teacher" + " " + "who" + " " + "lives" + " " + "in" + " " + "Berlin")
+
+//4
+let length = 16.78; // I think it is a number
+let surname = "Johnson"; // I think it is a string
+let car = ""; // I think it is also a string
+let isOpen = true; // I think it is a boolean
+console.log("length is a "+typeof length+", surname is a "+typeof surname+" ,car is a "+typeof car+" ,and isOpen is a "+typeof isOpen);
+
+//5
+let ageMark = "31";
+let ageJohn = 29 ;
+console.log("ageMark is a "+typeof ageMark+" , and ageJohn is a "+typeof ageJohn);
+
+
+//6
+console.log((ageJohn > ageMark)?"John is older than Mark: true":"Nope, Mark is older");
+
+//7
+var a = 1;
+var b = 2;
+var c = 1;
+console.log(a === b);
+console.log(a === c);
+console.log(b === a +c);
+
+
+//8
+let programmingLanguage = "Javascript";
+let isFun = true;
+
+console.log(programmingLanguage + " " + "is fun:" + " " + isFun);
