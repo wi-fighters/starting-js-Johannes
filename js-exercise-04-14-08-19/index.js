e788bd42da6e56fee4d2d5939dabf6b26ebb0fca
Sunthief <johannes.kneussel@googlemail.com>
2019-08-26 08:57:56 +0200

    Add some more exercises

--- js-exercise-04-14-08-19/index.js
+++ js-exercise-04-14-08-19/index.js
@@ -52,3 +52,38 @@ console.log(`${firstString[0]}${firstString}${firstString[0]}`);
 console.log(
   `${firstString.substring(7)}${firstString}${firstString.substring(7)}`
 );
+
+//10
+
+const computer = "ThinkPad"; // I declare a variable
+console.log(`${computer.toUpperCase()}`); // I transform the value of my variable into uppercase with the method ".toUpperCase()" and print it
+console.log(
+  `Does our string include the word Java? ${computer.includes("Java")}`
+);
+
+//11
+
+const myString = "JavaScript"; // I declare a variable and give it the value "JavaScript"
+console.log(
+  `${myString[myString.length - 1]}${myString.substring(
+    1,
+    myString.length - 1
+  )}${myString[0]}`
+); // Bit by bit. First of all we will explain ${myString[myString.length - 1]}. "myString.length" returns the total number of characters of the variable (which are 10). That means "[myString.length - 1]" is the same than "10 - 1" (which is "9"). Once we get that, we can continue: ${myString[myString.length - 1]} is the same as writing ${myString[9]} and this means just extracting the 8th character of the string: "t" (don't forget we start counting from nill). Same example here: ${myString.substring(1, myString.length - 1)}. "myString.length - 1" is equals to "10-1" (which is "9"). So it's the same as writing ${myString.substring(1, 9)} and this means we want to extract from the 2nd until the 8th character of the string because the 9th won't be included. This means it returns "avaScrip". ${myString[0]} just gets the very first character of the string, which is "J". So in the end, the console returns "tavaScripJ"
+
+// 12. Concatenation
+
+// Create 3 different variables about yourself using strings as values e.g.let firstName = "Maria".Print the sentence to the console using string interpolation. Example of output: `My name is Maria. I live in Berlin and I am a teacher`.
+
+const myName = "Johannes"; // I declare the first variable
+const myCity = "Berlin"; // I declare the second variable
+const myJob = "web developer"; // I declare the third variable
+console.log(`My name is ${myName}. I live in ${myCity} and I am a ${myJob}.`); // I print a string with the values of my variables in placeholders within a string literal
+
+//13
+
+// Declare a variable and assign the value the quick brown fox (lower case). Capitalize the first letter of that string.
+
+const pangram = "the quick brown fox"; // I declare a variable
+console.log(`${pangram[0].toUpperCase()}${pangram.substr(1)}`); // Bit by bit. First of all we will analyze ${pangram[0].toUpperCase()}. "pangram[0]" extracts the index 0 of our string, which means the first character of it: "t" (since we start counting from nill). Then we add ".toUpperCase()" in order to convert the "t" into "T". After that we go for ${pangram.substr(1)} which will return all the characters between the 2nd character of the string and the end of it (since we didn't write a second number telling the console where to stop). This returns "he quick brown fox". And both values together will return "The quick brown fox"
+console.log(pangram[0].toUpperCase() + pangram.substr(1)); // This is another way to do the same thing. We can just concatenate both values in order not to create a string literal between backticks `.....`
--- js-exercise-06-19-08-19/index.js
+++ js-exercise-06-19-08-19/index.js
@@ -1,8 +1,54 @@
+// 1. Lowest Number
+
+const Peter = 28;
+const John = 32;
+const Sarah = 27;
+const Hannah = 33;
+const Michael = 25;
+const friends = [Peter, John, Sarah, Hannah, Michael];
+
+console.log(
+  `The youngest friend in the group is ${Math.min(...friends)} years old.`
+);
+
+// 2. Highest Number
+
+console.log(
+  `The oldest friend in the group is ${Math.max(...friends)} years old.`
+);
+
+// 3. Rounding
+// 3.1. Round up
+
+const numberOne = 3321.32321;
+const numberTwo = 326.76;
+const numberThree = 76788.7;
+const numberFour = -9.78;
+const numberFive = 43.342;
+
+console.log(
+  `Rounding up the given numbers: ${Math.ceil(numberOne)}, ${Math.ceil(
+    numberTwo
+  )}, ${Math.ceil(numberThree)}, ${Math.ceil(numberFour)}, ${Math.ceil(
+    numberFive
+  )}`
+);
+
+// 3.2 Round down
+
+console.log(
+  `Rounding down the given numbers: ${Math.floor(numberOne)}, ${Math.floor(
+    numberTwo
+  )}, ${Math.floor(numberThree)}, ${Math.floor(numberFour)}, ${Math.floor(
+    numberFive
+  )}`
+);
+
 // 4. Dice Roll
 
-const minNum = Math.ceil(1);
-const maxNum = Math.floor(6);
+const minNum = 1;
+const maxNum = 6;
 
 console.log(Math.random());
 
-console.log(Math.ceil(Math.random()*maxNum));
+console.log(Math.ceil(Math.random() * maxNum));
