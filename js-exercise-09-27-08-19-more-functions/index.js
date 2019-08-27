f00338ea19aca5eeab25fe8567f0ae0335c9d2fd
Sunthief <johannes.kneussel@googlemail.com>
2019-08-27 10:44:55 +0200

    Minor fixes

---
+++ js-exercise-09-27-08-19-more-functions/index.js
@@ -0,0 +1,6 @@
+function tellFortune(numberChildren, partnerName, geoLocation, jobTitle) {
+  return `You will be a ${jobTitle} in ${geoLocation} and married to ${partnerName} with ${numberChildren} children.`;
+}
+console.log(tellFortune(3, "Steffi", "Berlin", "web developer"));
+console.log(tellFortune(2, "Sabine", "Hamburg", "psychologist"));
+console.log(tellFortune(4, "Karo", "Marburg", "teacher"));
