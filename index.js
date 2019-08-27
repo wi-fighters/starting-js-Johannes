6c653c43a095f2da69b90ede4adb8ffa7ede740d
Sunthief <johannes.kneussel@googlemail.com>
2019-08-27 09:48:48 +0200

    Moving some files

--- index.js
+++
@@ -1,6 +0,0 @@
-function tellFortune(children, partnerName, location, job) {
-  return `You will be a ${job} in ${location} and married to ${partnerName} with ${children} children.`;
-}
-console.log(tellFortune(3, "Steffi", "Berlin", "web developer"));
-console.log(tellFortune(2, "Sabine", "Hamburg", "psychologist"));
-console.log(tellFortune(4, "Karo", "Marburg", "teacher"));
