6e91fa5a5d735554d93115bfd9b0b04c705aaac0
Sunthief <johannes.kneussel@googlemail.com>
2019-09-03 13:50:48 +0200

    Add callback to function expressions

--- js-lesson-06-02-09-18-if-switch-ternary/func-expressions.js
+++ js-lesson-06-02-09-18-if-switch-ternary/func-expressions.js
@@ -23,3 +23,29 @@ function talk(person) {
     console.log(`I've never met a ${person} before.`);
   }
 }
+
+const talkPirate = function() {
+  return "ahoy";
+};
+const greetAnAustralian = function() {
+  return "oi";
+};
+
+function talkToJenna(howToTalk) {
+  return `${howToTalk()} Jenna!`;
+}
+
+console.log(talkToJenna(talkPirate));
+
+// Here's another way to write the same function,
+// using the accumulator pattern.
+// Breaking down the steps like this will be useful
+// as your funcs become more complex
+
+// function talkToJenna(howToTalk) {
+//     let greeting = '';
+
+//     greeting = `${howToTalk()} Jenna!`;
+
+//     return greeting;
+// }
